b8b3945df7d56db3db24849b2f159d468c29cef8
Guilherme Bento <guilherme.m.d.bento@gmail.com>
2019-01-08 09:57:51 +0100

    Added build vim reference file

--- .bash_profile
+++ .bash_profile
@@ -10,7 +10,7 @@ export PROMPT_COMMAND='history -a'
 # Load the shell dotfiles, and then some:
 # * ~/.path can be used to extend `$PATH`.
 # * ~/.extra can be used for other settings you don’t want to commit.
-for file in ~/.{path,bash_prompt,exports,aliases,functions,extra,deploy_backoffice}; do
+for file in ~/.{path,bash_prompt,exports,aliases,functions,extra}; do
     [ -r "$file" ] && [ -f "$file" ] && source "$file";
 done;
 unset file;
---
+++ scripts/build-vim.sh
@@ -0,0 +1,13 @@
+./configure --with-features=huge \
+            --enable-multibyte \
+	    --enable-rubyinterp=yes \
+	    --enable-pythoninterp=yes \
+	    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \ # pay attention here check directory correct
+	    --enable-python3interp=yes \
+	    --with-python3-config-dir=/usr/lib/python3.5/config \
+	    --enable-perlinterp=yes \
+	    --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --enable-terminal \
+            --with-x  \
